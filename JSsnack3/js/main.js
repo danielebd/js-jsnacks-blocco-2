f05515d598c6d08ddd0c3bc3f7c33f23fcd07d61
danielebd <danielebalebalestro@gmail.com>
2023-02-27 12:38:31 +0100

    JSsnack3

--- JSsnack3/js/main.js
+++ JSsnack3/js/main.js
@@ -1,2 +1,20 @@
 'use sctrict';
 
+/* Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati. */
+
+
+const listFirstname = ['daniele', 'marco', 'davide', 'merlino', 'federico'];
+const listLastname = ['a', 'b', 'c', 'd', 'e', 'f'];
+const listinvited = [];
+
+for( let i = 0; i < 3; i++) {
+    const name = Math.floor(Math.random() * listFirstname.length);
+    const secondName = Math.floor(Math.random() * listLastname.length);
+
+    console.log(name);
+
+    const newFullName = `${listFirstname[name]} ${listLastname[secondName]}`;
+    listinvited.push(newFullName);
+}
+
+console.log(listinvited);
