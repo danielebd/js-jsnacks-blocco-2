7eb1ccac2ab3342e5bfeed97b7f7d985725cccbf
danielebd <danielebalebalestro@gmail.com>
2023-03-01 11:02:45 +0100

    jsnack1

---
+++ JSsnack1/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+for(let i = 0; i < 5; i++){
+    const number = prompt('inserisci numero');
+    console.log(number);
+}
+
+let i = 0
+while(i < 5){
+    const number = prompt('inserisci numero');
+    console.log(number);
+    i++
+}
