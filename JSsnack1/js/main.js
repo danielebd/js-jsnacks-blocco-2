26397437769bfc9d4604bd8ef3d1aadd81cdbeb4
danielebd <danielebalebalestro@gmail.com>
2023-03-01 11:24:19 +0100

    JSsnack1

--- JSsnack1/js/main.js
+++ JSsnack1/js/main.js
@@ -1,13 +1,24 @@
 'use strict';
+let sum = 0;
 
-for(let i = 0; i < 5; i++){
-    const number = prompt('inserisci numero');
+for(let i = 1; i < 6; i++){
+    const number = Number(prompt('inserisci numero ' + i));
     console.log(number);
+    if(!isNaN(number)){
+        sum += number
+    }
 }
+alert('somma1=  ' + sum)
 
-let i = 0
-while(i < 5){
-    const number = prompt('inserisci numero');
+
+let sum2 = 0;
+let i = 1
+while(i < 6){
+    const number = Number(prompt('inserisci numero ' + i));
     console.log(number);
+    if(!isNaN(number)){
+        sum2 += number
+    }
     i++
 }
+alert('somma2= ' + sum2)
